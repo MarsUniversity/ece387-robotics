972340fa8df6bf03d1bdf022e1f44a72535d9756
Kevin <walchko@users.noreply.github.com>
2017-04-25 16:36:34 -0600

    Create hello.py

---
+++ code/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import print_statement
+from __future__ import division
+from math import pi  # get pi from the math library
+
+def main():
+    # this is a function called main, but it could have been called anything
+    print('hello!')  # you can use single quotes for strings
+    print('the value of pi is: {:.6f}'.format(pi))  # you can format output, this is a float with 6 decimal places
+    print("bye")  # you can use double quotes for strings
+
+
+if __name__ is '__main__':
+    main()
