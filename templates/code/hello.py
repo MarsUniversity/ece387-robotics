fad3d3c4808642713c3572fb8656a7c6e3d6efc1
Kevin <walchko@users.noreply.github.com>
2017-04-26 08:49:01 -0600

    Update hello.py

--- templates/code/hello.py
+++ templates/code/hello.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+#---------------------------------------------
+# Author: Doctor Who
+# License: MIT
 
-from __future__ import print_statement
-from __future__ import division
-from math import pi  # get pi from the math library
+from __future__ import print_statement  # bring in python 3 print function
+from __future__ import division         # use python 3 division
+from math import pi                     # get pi from the math library
+
 
 def main():
-    # this is a function called main, but it could have been called anything
+    """
+    This is a function called main, but the function could have been called anything.
+    This is a docstring and can be multi-line ... it should describe wheat your function
+    does.
+    """
     print('hello!')  # you can use single quotes for strings
     print('the value of pi is: {:.6f}'.format(pi))  # you can format output, this is a float with 6 decimal places
     print("bye")  # you can use double quotes for strings
