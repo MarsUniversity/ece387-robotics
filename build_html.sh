c788d0191e0d9e7ff78076571105cc6dea74c598
walchko <walchko@users.noreply.github.com>
2017-09-09 13:55:37 -0600

    test

--- build_html.sh
+++ build_html.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# clean house
 if [[ -d "www" ]]; then
 	rm -fr www
 fi
 
+# re-create empty directory
 mkdir -p www
-cd www
-cat <<EOF > index.html
-hi
-EOF
+
+# dummy test
+# cd www
+# cat <<EOF > index.html
+# hi
+# EOF
+
+WWW_PATH=../../../www
+
+cd docs/handouts/syllabus
+pandoc syllabus.md -o index.html
+mv index.html ${WWW_PATH}
