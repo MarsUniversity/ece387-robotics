4c8818fb5472db1b97762841e84717df884972db
walchko <walchko@users.noreply.github.com>
2017-09-11 18:32:23 -0600

    latex fix

--- website/labs/build.sh
+++ website/labs/build.sh
@@ -12,6 +12,6 @@ rm -f *.pdf
 for DOC in 'lab1' 'lab2' 'lab3' 'lab4' 'lab5' 'final_project'
 do
 	echo ">" ${DOC}
-	pandoc ${DOC}.md -V geometry:margin=1in -s -o ${DOC}.pdf
+	pandoc ${DOC}.md -V geometry:margin=1in -s --latex-engine=pdflatex -o ${DOC}.pdf
 	echo "-------------------"
 done
