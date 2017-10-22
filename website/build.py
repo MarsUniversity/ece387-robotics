56dfeb04daa49c6a2376085fd86ab7899cb8ec12
Kevin <walchko@users.noreply.github.com>
2017-10-22 00:43:23 -0600

    update

--- website/build.py
+++ website/build.py
@@ -157,10 +157,10 @@ def main():
 		'block_4_mobile_robotics'
 	]
 
-	# for blk, j, p in zip(blocks, jup, ppt):
-	# 	build_block(blk, j, p)
+	for blk, j, p in zip(blocks, jup, ppt):
+		build_block(blk, j, p)
 
-	# build_guides()
+	build_guides()
 
 	# syllabus -----------------------------------------------------------------
 	run('cp pandoc.css www')
