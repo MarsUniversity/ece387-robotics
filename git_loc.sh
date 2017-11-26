20e7679e1d6bde4b411f0044dad81b14670d2510
Kevin <walchko@users.noreply.github.com>
2017-11-25 23:01:52 -0700

    python

--- git_loc.sh
+++ git_loc.sh
@@ -6,7 +6,7 @@ IMU="nxp_imu"
 CV="opencvutils"
 FAKE="fake_rpi"
 NAV="ins_nav"
-
+ECE="https://github.com/MarsUniversity/ece387"
 
 for REPO in ${CREATE} ${IMU} ${CV} ${FAKE} ${NAV}
 do
@@ -15,3 +15,5 @@ do
   echo "=========================================="
   ./loc.sh ${BASE}${REPO}
 done
+
+./loc.sh ${ECE}
